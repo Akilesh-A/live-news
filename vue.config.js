8488d66c985c08f393123e88babfd8cc33039458
Akilesh-A <akileshanand21@gmail.com>
2024-07-09 16:36:42 +0530

    updated deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: 'dist', 
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   outputDir: 'dist', 
+// })
+
+module.exports = {
+  outputDir: 'build',  // Or 'dist', adjust based on your project configuration
+  // Other configurations...
+};
